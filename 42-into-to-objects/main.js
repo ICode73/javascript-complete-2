c571c1a6b2217e125bcdf86e1ded60dc1815f295
ADonovanWP <anthonyrdonovan1@gmail.com>
2023-09-30 12:00:59 -0500

    Had an introduction to objects, great refresher

--- 42-into-to-objects/main.js
+++ 42-into-to-objects/main.js
@@ -3,7 +3,18 @@
 /*
 
 
-
+* INTRODUCTION TO OBJECTS
 
 
 */
+
+//* Object literal syntax
+
+const fred = {
+  firstName: "Fred",
+  lastName: "Flintstone",
+  age: 2037 - 1950,
+  job: "miner",
+  friends: ["Barney", "Wilma", "Betty"],
+};
+console.log(fred);
